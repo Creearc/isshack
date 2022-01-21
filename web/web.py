a453873af4857a9185f74f2408465c9935b430fe
Alexandr <knxandr@rambler.ru>
2022-01-21 22:28:03 +0500

    Update web.py

--- web/web.py
+++ web/web.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
   context = zmq.Context()
   socket = context.socket(zmq.SUB)
   socket.connect("tcp://{}:{}".format(ip, 5001))
-  topicfilter = 10001
+  topicfilter = b'10001'
   socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
   
   app.run(host='0.0.0.0', port=58800, debug=False, threaded=True, use_reloader=False)
