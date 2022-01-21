ea6639b1f3467cba4c0d92a500eb03543e27e3ca
Alexandr <knxandr@rambler.ru>
2022-01-21 23:56:44 +0500

    weights

---
+++ key_point/pose_detection_5.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNetFromDarknet('key_point/model_5/yolov4.cfg',
+                                 'key_point/model_5/yolov4.weights')
+layer = net.getLayerNames()
+layer = [layer[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+def detect(frame):
+    (H, W) = img.shape[:2]
+    blob = cv2.dnn.blobFromImage(img, 1 / 255, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    layer_outs = net.forward(layer)
+    boxes = list()
+    confidences = list()
+    class_ids = list()
+    # Get result
+    for output in layer_outs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > thresh:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (center_x, center_y, width, height) = box.astype("int")
+                x = int(center_x - (width / 2))
+                y = int(center_y - (height / 2))
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, thresh, nms)
+    if len(indexes) > 0:
+        for i in indexes.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            
+    return img
+    
+
