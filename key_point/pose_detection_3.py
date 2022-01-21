7d87d18f296a756173e6b6181fb2008b35db9ac7
Alexandr <knxandr@rambler.ru>
2022-01-21 23:43:21 +0500

    3

---
+++ key_point/pose_detection_3.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+from imutils.object_detection import non_max_suppression
+
+HOGCV = cv2.HOGDescriptor()
+HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+def detect(frame):
+    rects, weights = HOGCV.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    c = 1
+    for x, y, w, h in pick:
+        cv2.rectangle(frame, (x, y), (w, h), (139, 34, 104), 2)
+        cv2.rectangle(frame, (x, y - 20), (w,y), (139, 34, 104), -1)
+        cv2.putText(frame, f'P{c}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+        c += 1
+
+    cv2.putText(frame, f'Total Persons : {c - 1}', (20, 450), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255,255), 2)
+
+    return frame
+    
+
--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 import pickle
 
-from key_point import pose_detection_2 as detector
+from key_point import pose_detection_3 as detector
 
 
 def server_thread():
