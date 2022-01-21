596157bdc82f6e8fff3243b56ab94f8969ba8160
Alexandr <knxandr@rambler.ru>
2022-01-21 21:05:56 +0500

    img

--- web/web.py
+++ web/web.py
@@ -1,15 +1,55 @@
+import threading
+import time
 import zmq
+import numpy as np
+import cv2
 
-def send(socket, tmp):
-  tmp = tmp.copy()
-  finished = False
-  while not finished:
-    try:
-      print('send start')
-      msg = pickle.dumps(tmp)
-      socket.send(msg, zmq.NOBLOCK)
-      socket.recv()     
-      finished = True
-    except:
-      time.sleep(5.0)
-  print('send finish')
+from flask import Response
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+
+@app.route("/video_feed")
+def video_feed():
+  return Response(generate(),
+                    mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+def generate():
+  global socket
+  while True:
+    socket.send_string('img$0', zmq.NOBLOCK)
+    msg = socket.recv()
+    img = pickle.loads(msg)
+    if img is None:
+      time.sleep(0.1)
+      continue
+    (flag, encodedImage) = cv2.imencode(".jpg", img)
+    if not flag:
+      time.sleep(0.1)
+      continue
+    yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+            bytearray(encodedImage) + b'\r\n')
+
+def main_thread():
+  while True:
+    pass
+
+
+if __name__ == '__main__':
+  lock = threading.Lock()
+
+  img = None
+
+  ip = '127.0.0.1'
+
+  context = zmq.Context()
+  socket = context.socket(zmq.REQ)
+  socket.connect("tcp://{}:{}".format(ip, 5001))
+  
+  app.run(host='0.0.0.0', port=58800, debug=False, threaded=True, use_reloader=False)
