81f5f5bf51b17bb962eb301a18d6781ccd17b6e9
Alexandr <knxandr@rambler.ru>
2022-01-22 19:05:55 +0500

    Update worker_final.py

--- final/worker_final.py
+++ final/worker_final.py
@@ -41,7 +41,7 @@ def main_thread():
       buf_sec = 0
       output_file = open('results/fight_result.txt', 'w')
       
-      for i in range(0, frame_count, 240):
+      for i in range(0, frame_count, 10):
         print(i)
         vid_capture.set(1, i)
         _, frame = vid_capture.read()
