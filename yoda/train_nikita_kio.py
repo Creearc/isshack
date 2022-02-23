f21fec87f53484479cb6ab2e617088f4b18feb62
nlfamlv <75396339+nlfamlv@users.noreply.github.com>
2022-02-23 22:39:16 +0500

    relu

--- yoda/train_nikita_kio.py
+++ yoda/train_nikita_kio.py
@@ -112,10 +112,10 @@ class NClassifier(nn.Module):
         self.sig = nn.Sigmoid()
         
     def forward(self, inputs):
-        x = self.relu(self.layer_1(inputs))
+        x = self.tanh(self.layer_1(inputs))
         x = self.batchnorm(x)
-        x = self.tanh(self.layer_3(x))
-        x = self.tanh(self.layer_5(x))
+        x = self.relu(self.layer_3(x))
+        x = self.relu(self.layer_5(x))
         x = self.dropout(x)
         x = self.sig(self.layer_out(x))
         
