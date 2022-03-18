be9bb318031409a7066d3f58f43968345796804f
Alexandr <knxandr@rambler.ru>
2022-03-18 20:48:42 +0500

    zmq

--- yoda/final/web.py
+++ yoda/final/web.py
@@ -1,14 +1,11 @@
 import threading
 import time
-import zmq
-import numpy as np
-import cv2
-
-import pickle
 
 from flask import Response
 from flask import Flask
 from flask import render_template
+
+import zmq_module
 
 app = Flask(__name__)
 
@@ -23,16 +20,8 @@ def video_feed():
                     mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 def generate():
-  global socket
   while True:
-    try:
-      socket.send_string('img$0', zmq.NOBLOCK)
-      msg = socket.recv()
-    except Exception as e:
-      print(e)
-      continue
-    
-    img = pickle.loads(msg)
+    img = rec.get_img()
     if img is None:
       time.sleep(0.1)
       continue
@@ -43,20 +32,9 @@ def generate():
     yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
             bytearray(encodedImage) + b'\r\n')
 
-def main_thread():
-  while True:
-    pass
-
 
 if __name__ == '__main__':
-  lock = threading.Lock()
-
-  img = None
-
-  ip = '127.0.0.1'
-
-  context = zmq.Context()
-  socket = context.socket(zmq.REQ)
-  socket.connect("tcp://{}:{}".format(ip, 5001))
+  rec = zmq_module.ZMQ_receiver('192.168.68.200', 5005)
+  rec.run()
   
   app.run(host='0.0.0.0', port=58800, debug=False, threaded=True, use_reloader=False)
